34e88c071fc80cb8960d9cdb9580c4ff9f253318
Davi Alexandre <davi@davialexandre.com.br>
2018-03-04 00:12:21 -0300

    Make it possible to create and load a Profile from a Profiler

---
+++ src/DaviAlexandre/DebugToolbar/Profile.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace DaviAlexandre\DebugToolbar;
+
+
+use DaviAlexandre\DebugToolbar\DataCollector\DataCollectorInterface;
+
+class Profile {
+
+  private $identifier;
+  private $time;
+  private $url;
+  private $method;
+  private $collectors = [];
+
+  public function __construct($identifier) {
+    $this->identifier = $identifier;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getIdentifier() {
+    return $this->identifier;
+  }
+
+  /**
+   * @return \DateTimeImmutable
+   */
+  public function getTime() {
+    return $this->time;
+  }
+
+  /**
+   * @param \DateTimeImmutable $time
+   */
+  public function setTime(\DateTimeImmutable $time) {
+    $this->time = $time;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getUrl() {
+    return $this->url;
+  }
+
+  /**
+   * @param mixed $url
+   */
+  public function setUrl($url) {
+    $this->url = $url;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getMethod() {
+    return $this->method;
+  }
+
+  /**
+   * @param mixed $method
+   */
+  public function setMethod($method) {
+    $this->method = $method;
+  }
+
+  public function addCollector(DataCollectorInterface $collector) {
+    $this->collectors[$collector->getName()] = $collector;
+  }
+
+  public function getCollectors() {
+    return $this->collectors;
+  }
+}
--- src/DaviAlexandre/DebugToolbar/Profiler.php
+++ src/DaviAlexandre/DebugToolbar/Profiler.php
@@ -7,14 +7,60 @@ use DaviAlexandre\DebugToolbar\DataCollector\DataCollectorInterface;
 class Profiler {
 
   private $collectors = [];
+  private $profileIdentifier;
+  private $paths;
+
+  public function __construct(\CRM_Core_Resources $resources, \Civi\Core\Paths $paths) {
+    $resources->addSetting([
+      'debug_toolbar_profile_identifier' => $this->createProfileIdentifier()
+    ]);
+
+    $this->paths = $paths;
+  }
 
   public function addDataCollector(DataCollectorInterface $collector) {
     $this->collectors[] = $collector;
   }
 
   public function collect() {
+    $profile = new Profile($this->createProfileIdentifier());
+    $profile->setTime(new \DateTimeImmutable());
+    $profile->setUrl(\CRM_Utils_Array::value('REQUEST_URI', $_SERVER));
+    $profile->setMethod(\CRM_Utils_Array::value('REQUEST_METHOD', $_SERVER));
+
     foreach ($this->collectors as $collector) {
       $collector->collect();
+      $profile->addCollector($collector);
     }
+
+    return $profile;
+  }
+
+  public function saveProfile(Profile $profile) {
+    $profilePath = $this->getProfilePath($profile->getIdentifier());
+    file_put_contents($profilePath, serialize($profile));
+  }
+
+  public function loadProfile($identifier) {
+    $profilePath = $this->getProfilePath($identifier);
+
+    return unserialize(file_get_contents($profilePath));
+  }
+
+  private function getProfilePath($identifier) {
+    return $this->paths->getPath('[civicrm.files]/debug_toolbar/profiles/'.$identifier);
+  }
+
+  private function createProfileIdentifier() {
+    if (!$this->profileIdentifier) {
+      $this->profileIdentifier = substr(hash('sha256', uniqid(mt_rand(), true)), 0, 6);
+    }
+
+    return $this->profileIdentifier;
+  }
+
+  public function __destruct() {
+    $profile = $this->collect();
+    $this->saveProfile($profile);
   }
 }
