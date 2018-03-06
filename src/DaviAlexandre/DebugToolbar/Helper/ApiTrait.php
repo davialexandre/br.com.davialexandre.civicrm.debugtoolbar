040fef593fd47d2ce0941056f3d2f6401bd3ab98
Davi Alexandre <davi@davialexandre.com.br>
2018-03-05 22:27:01 -0300

    Allow passing params to the API helpers

--- src/DaviAlexandre/DebugToolbar/Helper/ApiTrait.php
+++ src/DaviAlexandre/DebugToolbar/Helper/ApiTrait.php
@@ -5,14 +5,14 @@ namespace DaviAlexandre\DebugToolbar\Helper;
 
 trait ApiTrait {
 
-  private function apiGet($entity) {
-    $response = civicrm_api3($entity, 'get');
+  private function apiGet($entity, $params = []) {
+    $response = civicrm_api3($entity, 'get', $params);
 
     return empty($response['values']) ? [] : $response['values'];
   }
 
-  private function apiGetFirst($entity) {
-    $response = $this->apiGet($entity);
+  private function apiGetFirst($entity, $params = []) {
+    $response = $this->apiGet($entity, $params);
 
     return reset($response);
   }
